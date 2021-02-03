be76cd1c1128537500df13267365eed86ced8ae0
Ondrej Sika <ondrej@ondrejsika.com>
2021-02-03 07:53:37 +0100

    Add demo data (docker-compose & nodes in TF)

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -5,6 +5,9 @@ variable "cloudflare_token" {}
 variable "vm_count" {
   default = 1
 }
+variable "demo_data_vm_count" {
+  default = 3
+}
 
 provider "digitalocean" {
   token = var.digitalocean_token
@@ -69,5 +72,79 @@ resource "cloudflare_record" "droplet_wildcard" {
   name   = "*.prom${count.index}"
   value  = cloudflare_record.droplet[count.index].hostname
   type   = "CNAME"
+  proxied = false
+}
+
+# Permanent Prometheus VM
+
+resource "digitalocean_droplet" "prom" {
+  image  = "docker-18-04"
+  name   = "prom"
+  region = "fra1"
+  size   = "s-1vcpu-2gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.ondrejsika.id
+  ]
+  user_data = <<-EOF
+  #cloud-config
+  ssh_pwauth: yes
+  password: asdfasdf2021
+  chpasswd:
+    expire: false
+  runcmd:
+    - ufw disable
+    - docker run -d --net="host" --pid="host" -v "/:/host:ro,rslave" quay.io/prometheus/node-exporter:latest --path.rootfs=/host
+  EOF
+}
+
+resource "cloudflare_record" "prom" {
+  domain = "sikademo.com"
+  name   = "prom"
+  value  = digitalocean_droplet.prom.ipv4_address
+  type   = "A"
+  proxied = false
+}
+
+# Demo Data
+
+resource "digitalocean_droplet" "demo-data" {
+  count = var.demo_data_vm_count
+
+  image  = "docker-18-04"
+  name   = "prom-demo-data${count.index}"
+  region = "fra1"
+  size   = "s-1vcpu-2gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.ondrejsika.id
+  ]
+  user_data = <<-EOF
+  #cloud-config
+  ssh_pwauth: yes
+  password: asdfasdf2021
+  chpasswd:
+    expire: false
+  runcmd:
+    - ufw disable
+    - docker run -d --net="host" --pid="host" -v "/:/host:ro,rslave" quay.io/prometheus/node-exporter:latest --path.rootfs=/host
+  EOF
+}
+
+resource "cloudflare_record" "demo-data" {
+  count = var.demo_data_vm_count
+
+  domain = "sikademo.com"
+  name   = "prom-demo-data${count.index}"
+  value  = digitalocean_droplet.demo-data[count.index].ipv4_address
+  type   = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "demo-data_all" {
+  count = var.demo_data_vm_count
+
+  domain = "sikademo.com"
+  name   = "prom-demo-data-all"
+  value  = digitalocean_droplet.demo-data[count.index].ipv4_address
+  type   = "A"
   proxied = false
 }
