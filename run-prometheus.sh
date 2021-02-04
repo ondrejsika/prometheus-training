abec63f57c3f459ef31b0012ab9a7d4d50540be7
Ondrej Sika <ondrej@ondrejsika.com>
2021-02-04 09:27:46 +0100

    run-prometheus.sh - Enable web lifecycle / reloads (CLI arg --web.enable-lifecycle)

--- run-prometheus.sh
+++ run-prometheus.sh
@@ -2,5 +2,4 @@
 
 CONFIG_FILE=${1?Prometheus config file required (as first parameter of ./run-prometheus.yml}
 
-prometheus --config.file=$CONFIG_FILE
-
+prometheus --config.file=$CONFIG_FILE --web.enable-lifecycle
