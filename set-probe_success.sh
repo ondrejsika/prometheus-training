9f901bb43324c8a84e2885c52f562b7d064e46b1
Ondrej Sika <ondrej@ondrejsika.com>
2025-06-04 16:40:33 +0200

    feat(set-probe_success.sh): Add $PUSH_PROXY_URL support

--- set-probe_success.sh
+++ set-probe_success.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-cat <<EOF | curl --data-binary @- http://prom.sikademo.com:9091/metrics/job/demo/instance/demo/app/$1
+PUSH_PROXY_URL=${PUSH_PROXY_URL:-http://prom.sikademo.com:9091}
+
+cat <<EOF | curl --data-binary @- $PUSH_PROXY_URL/metrics/job/demo/instance/demo/app/$1
 # TYPE probe_success gauge
 # HELP probe_success Service is up and running
 probe_success $2
